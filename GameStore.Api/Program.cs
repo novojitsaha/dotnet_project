bb8c0c6bd4ba43f433f6c9fe89f2dbfab9e1a53d
Novojit Saha <n.saha@student.utwente.nl>
2026-03-29 15:24:40 +0200

    Add DTOs and endpoints

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,6 +1,13 @@
+
+using GameStore.Api.Endpoints;
+
+
+
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGamesEnpoints();
+
 
 app.Run();
