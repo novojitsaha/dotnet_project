1c300b73f2ac1b2cdfe54e66eee97471bb807061
Novojit Saha <n.saha@student.utwente.nl>
2026-03-29 20:51:48 +0200

    Refactor GamesEndpoints to use async methods for database operations and improve data retrieval with DTOs

--- GameStore.Api/Endpoints/GamesEndpoints.cs
+++ GameStore.Api/Endpoints/GamesEndpoints.cs
@@ -1,7 +1,7 @@
 using GameStore.Api.Data;
 using GameStore.Api.Dtos;
 using GameStore.Api.Models;
-
+using Microsoft.EntityFrameworkCore;
 namespace GameStore.Api.Endpoints;
 
 public static class GamesEndpoints
@@ -15,26 +15,42 @@ public static class GamesEndpoints
         var group = app.MapGroup("/games");
 
         // GET /games
-        group.MapGet("/", (GameStoreContext dbContext) =>
-        {
-            return dbContext.Games.ToList();
-        });
+        group.MapGet("/", async (GameStoreContext dbContext)
+            => await dbContext.Games
+            .Include(game => game.Genre)
+            .Select(game => new GameDto(
+                game.Id,
+                game.Name,
+                game.Genre.Name,
+                game.Price,
+                game.ReleaseDate
+        ))
+            .AsNoTracking()
+            .ToListAsync());
 
 
 
         // GET /games/{id}
-        group.MapGet("/{id}", (int id, GameStoreContext dbContext) =>
+        group.MapGet("/{id}", async (int id, GameStoreContext dbContext) =>
         {
 
-            var game = dbContext.Games.Find(id);
+            var game = await dbContext.Games.FindAsync(id);
 
-            return game is null ? Results.NotFound() : Results.Ok(game);
+            return game is null ? Results.NotFound() : Results.Ok(
+                new GameDetailsDto(
+                    game.Id,
+                    game.Name,
+                    game.GenreId,
+                    game.Price,
+                    game.ReleaseDate
+                )
+            );
         })
             .WithName(GetGameEndpointName);
 
 
         // POST /games
-        group.MapPost("/", (CreateGameDto newGame, GameStoreContext dbContext) =>
+        group.MapPost("/", async (CreateGameDto newGame, GameStoreContext dbContext) =>
         {
 
             Game game = new()
@@ -46,7 +62,7 @@ public static class GamesEndpoints
             };
 
             dbContext.Games.Add(game);
-            dbContext.SaveChanges();
+            await dbContext.SaveChangesAsync();
 
             GameDetailsDto gameDto = new(
                 game.Id,
